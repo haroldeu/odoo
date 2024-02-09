b173f7635cd73fdd52f9521dcccc0f8655877237
Harold <haroldeuu@gmail.com>
2024-02-09 10:18:54 +0800

    Add input

--- main.py
+++ main.py
@@ -3,7 +3,6 @@
 # Press Shift+F10 to execute it or replace it with your code.
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
 
-import functions
 import time
 
 now = time.strftime("%b, %d, %Y %H:%M:%S")
@@ -18,6 +17,6 @@ def print_hi(name):
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('PyCharm')
+    print_hi(input("What is your name? "))
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
